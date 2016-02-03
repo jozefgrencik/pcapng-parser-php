3064814f404ebabc531087ea10f14386210ca82c
Joso <joso@example.com>
2016-02-03 20:58:23 +0100

    Add .gitignore .gitattributes and license

---
+++ src/PcapngParser.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace jozefgrencik;
+
+class PcapngParser {
+
+    const VERSION = 0.1;
+
+
+    public function parse($raw) {
+
+    }
+
+    public function parseFile($filePath) {
+
+    }
+
+}
